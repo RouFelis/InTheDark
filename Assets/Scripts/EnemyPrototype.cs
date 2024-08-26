bee3c227d0ebef0680308a523d8dbca78207c3b1
RouFelis <RouTheCat@gmail.com>
2024-08-26 15:18:52 +0900

    현재까지 작업

---
+++ Assets/Scripts/EnemyPrototype.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace InTheDark
+{
+    public class EnemyPrototype : MonoBehaviour
+    {
+        private void Update()
+        {
+            Debug.DrawRay(transform.position, transform.forward * 5.0f, Color.magenta);
+        }
+    }
+}
---
+++ Assets/Scripts/LightSource.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace InTheDark
+{
+    public class LightSource : MonoBehaviour
+    {
+        [SerializeField] 
+        private float _angle;
+
+        [SerializeField] 
+        private float _distance;
+
+        public float Angle => _angle;
+        public float Distance => _distance;
+    }
+}
